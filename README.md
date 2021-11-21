fb058c4409d770a51c169219da8fe7333dfe9525
seyfkutluk <seyfkutluk@gmail.com>
2021-11-21 03:57:58 +0300

    MySql İntegrated To login And Register

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ The project is about to find a best match for individuals to find a job in EU ar
 Cosidering the data we obtain in https://ec.europa.eu/eurostat/en/web/lfs/data/database specifically LFS series - detailed quarterly survey results (from 1998 onwards) (lfsq)	
 By considering population, employment, sex, profession area, education level, unemployment rate we can estimate the probability of having a job in countries.
 Also, this data can be used for starting a business/self employment, temporary jobs, full time/part time employment while considering working hours.
+
+
+To reach the project you can visit https://blg-317.herokuapp.com/
--- server.py
+++ server.py
@@ -1,20 +1,93 @@
+from logging import error
 from flask import Flask, render_template
+from flask import redirect, url_for, request, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 import os
+import re
+
+from werkzeug.utils import redirect
 
 app = Flask(__name__, static_folder=os.path.abspath('templates/static'))
 
+app.secret_key= 'root'
 
-@app.route("/")
+app.config['MYSQL_HOST'] ='153.92.220.151'
+app.config['MYSQL_USER'] ='u413789411_seyfkutluk'
+app.config['MYSQL_PASSWORD'] ='47.Antartika'
+app.config['MYSQL_DB'] ='u413789411_blg'
+
+mysql=MySQL(app)
+
+
+@app.route("/home")
 def home_page():
     return render_template("index.html")
 
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
 
-@app.route("/register")
-def register_page():
-    return render_template("register.html")
+@app.route('/login')
+@app.route('/',methods=['GET','POST'])
+def login_page():
+    error=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s',(username,password,))
+        account = cursor.fetchone()
+
+        if account:
+            session['loggedin'] = True
+            session['username'] = account['username']
+            session['id'] = account['id']
+            error = 'Logged in successfully !'
+            return render_template('index.html',error = error)
+
+        else:
+            error = '!!!!Incorrect username or password!!!!!!!'
+            
+    return render_template('login.html',error =error)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login_page'))
+
+@app.route('/register', methods =['GET', 'POST'])
+def register():
+    message = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username=request.form['username']
+        email = request.form['email']
+        password=request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s', (username, ))
+        account = cursor.fetchone()
+
+        if account:
+            message = 'Account already exists !'
+
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            message = 'Invalid email address !'
+
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            message = 'Username must contain only characters and numbers !'
+
+        elif not username or not password or not email:
+            message = 'Please fill out the form !'
+
+        else:
+            cursor.execute('INSERT INTO accounts VALUES (NULL, % s, % s, % s)', (username, password, email, ))
+            mysql.connection.commit()
+            message ='You have successfully registered'
+
+
+    elif request.method == 'POST':
+        message = 'Please fill out the form !'
+
+    return render_template('register.html', message = message)
 
 
 @app.route("/password")
